2fa71cf6bbb4d5d32b03c65ae1c382c87c6115b6
Reid Mitchell <reidmitchell1@gmail.com>
2018-01-31 13:06:42 -0800

    Got rewrite working, tests passing

--- src2.js
+++ src2.js
@@ -1,57 +1,94 @@
-let cache = {};
 let rules = [];
+let cache = {};
 
-// we can determine the classname much earlier
-// (outside of props->) by just using a counter
-// of calls to rule() instead of rules.legth.
-// Since one call to rule() can result in multiple
-// rules, but the class name should be the same
-// for all (e.g. ".cn", ".cn:hover", "@media{.cn}")
+const mergeKey = (oldKey, newKey) =>
+  oldKey + (oldKey.indexOf('@media') > -1
+    ? newKey.replace('@media', ' and')
+    : newKey.replace(/&:/g, ':'));
 
-// wait can we? wat about diff props?
+const newClassName = () =>
+  'cls_' + rules.length;
 
-export const rule = obj => {
-  const defs = [];
+const sortedKeys = obj =>
+  Object.keys(obj).sort((a, b) => a.length - b.length);
+
+const collectDefs = (obj, defs, level = '', cacheKey = '') => {
   let isStatic = true;
-  let staticDefs;
 
   Object.keys(obj).map(key => {
+    defs[level] = defs[level] || [];
     const val = obj[key];
     const cssKey = key.replace(/[A-Z]/g, '-$&').toLowerCase();
 
     const type = typeof val;
     if (type === 'function') {
       isStatic = false;
-      defs.push(props => cssKey + ':' + val(props) + ';');
+      defs[level].push(props => cssKey + ':' + val(props) + ';');
+      cacheKey += key + ':<func>;';
     } else if (type === 'object') {
-
+      const { isStatic: nestedIsStatic, cacheKey: nestedCacheKey } = collectDefs(val, defs, mergeKey(level, key));
+      isStatic = isStatic && nestedIsStatic;
+      cacheKey += nestedCacheKey;
     } else {
-      defs.push(cssKey + ':' + val + ';');
+      defs[level].push(cssKey + ':' + val + ';');
+      cacheKey += defs[level];
     }
+
+    if (!defs[level].length) delete defs[level];
   });
 
-  if (isStatic) {
-    staticDefs = defs.join('');
-  }
-
-  const func = props => {
-    const ruleDefs = isStatic ? staticDefs :
-      defs.map(def =>
-        typeof def === 'function' ? def(props) : def)
-          .join('');
-
-    if (cache[ruleDefs]) return cache[ruleDefs];
-
-    const className = 'cls_' + rules.length;
-    rules.push(`.${className}{${ruleDefs}}`);
-
-    return cache[ruleDefs] = className;
-  };
-
-  return isStatic ? func() : func;
+  return { defs, isStatic, cacheKey };
 };
 
-export const css = () => rules.sort().join('\n');
+export const rule = obj => {
+  const { defs, isStatic, cacheKey } = collectDefs(obj, {});
+
+  if (isStatic) {
+    if (cache[cacheKey]) return cache[cacheKey];
+    const cn = cache[cacheKey] = newClassName();
+
+    sortedKeys(defs).forEach(key => {
+      const values = defs[key].join('');
+
+      const formattedRule = key.indexOf('@') > -1
+        ? `${key}{.${cn}{${values}}}`
+        : `.${cn}${key}{${values}}`
+
+      rules.push(formattedRule);
+    });
+
+    return cn;
+  }
+
+  const ruleGenerators = sortedKeys(defs).map(key => {
+    const vals = defs[key];
+    if (key.indexOf('@') > -1) {
+      return (cn, props) => `${key}{.${cn}{${vals.map(val =>
+        typeof val === 'function' ? val(props) : val).join('')
+      }}}`;
+    }
+
+    return (cn, props) => `.${cn}${key}{${vals.map(val =>
+      typeof val === 'function' ? val(props) : val).join('')
+    }}`;
+  });
+
+  return props => {
+    const dynamicCacheKey = cacheKey + JSON.stringify(props);
+    if (cache[dynamicCacheKey]) return cache[dynamicCacheKey];
+
+    const cn = cache[dynamicCacheKey] = newClassName();
+
+    ruleGenerators.forEach(f => {
+      rules.push(f(cn, props));
+    });
+
+    return cn;
+  };
+};
+
+export const css = () =>
+  rules.join('\n');
 
 export const reset = () => {
   rules = [];
--- test.js
+++ test.js
@@ -58,17 +58,106 @@ test('dynamic rules', () => {
   );
 });
 
-test('nested rules w/pseudoselectors', () => {
+test('static nested rules w/pseudoselectors', () => {
   const cn0 = rule({
     color: 'red',
     '&:hover': {
       color: 'green'
+    },
+    '&:focus': {
+      '&:before': {
+        color: 'blue'
+      }
     }
   });
 
   expect(cn0).toBe('cls_0');
   expectCss(
     '.cls_0{color:red;}',
-    '.cls_0:hover{color:green;}'
+    '.cls_0:hover{color:green;}',
+    '.cls_0:focus:before{color:blue;}'
+  );
+});
+
+test('dynamic nested rules w/pseudoselectors', () => {
+  const rule0 = rule({
+    color: 'red',
+    width: props => props.size + '%',
+    '&:hover': {
+      color: 'green'
+    },
+    '&:focus': {
+      '&:before': {
+        color: 'blue',
+        background: props => props.bg
+      }
+    }
+  });
+
+  const cn0 = rule0({ bg: 'purple', size: 47 });
+
+  expect(cn0).toBe('cls_0');
+  expectCss(
+    '.cls_0{color:red;width:47%;}',
+    '.cls_0:hover{color:green;}',
+    '.cls_0:focus:before{color:blue;background:purple;}'
+  );
+});
+
+test('static nested rules w/ media queries', () => {
+  const cn0 = rule({
+    color: 'green',
+    fontSize: '10px',
+    '@media (max-width: 99px)': {
+      color: 'blue',
+      width: '47%'
+    },
+    '@media print': {
+      color: 'red',
+      '@media (max-width: 28px)': {
+        color: 'yellow'
+      }
+    }
+  });
+
+  expect(cn0).toBe('cls_0');
+
+  expectCss(
+    '.cls_0{color:green;font-size:10px;}',
+    '@media print{.cls_0{color:red;}}',
+    '@media (max-width: 99px){.cls_0{color:blue;width:47%;}}',
+    '@media print and (max-width: 28px){.cls_0{color:yellow;}}'
+  );
+});
+
+test('dynamic nested rules w/ media queries', () => {
+  const rule0 = rule({
+    color: props => props.defaultColor,
+    fontSize: '10px',
+    '@media (max-width: 99px)': {
+      color: props => props.minWidthColor,
+      width: '47%'
+    },
+    '@media print': {
+      color: 'red',
+      '@media (max-width: 28px)': {
+        color: props => props.smallWidthColor
+      }
+    }
+  });
+
+  const cn0 = rule0({
+    defaultColor: 'purple',
+    minWidthColor: 'orange',
+    smallWidthColor: 'teal'
+  });
+
+  expect(cn0).toBe('cls_0');
+
+  expectCss(
+    '.cls_0{color:purple;font-size:10px;}',
+    '@media print{.cls_0{color:red;}}',
+    '@media (max-width: 99px){.cls_0{color:orange;width:47%;}}',
+    '@media print and (max-width: 28px){.cls_0{color:teal;}}'
   );
 });
