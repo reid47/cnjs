8e2eb1d641e7cca879e8058b44eeecea7ad8eda5
Reid Mitchell <reidmitchell1@gmail.com>
2018-03-08 20:12:58 -0800

    Making three entry points in dist: client/server/turnstyle

--- rollup.config.js
+++ rollup.config.js
@@ -3,23 +3,34 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import uglify from 'rollup-plugin-uglify';
 import fileSize from 'rollup-plugin-filesize';
-import replace from 'rollup-plugin-replace';
+
+const common = ({ inputFile, outputFile }) => ({
+  input: inputFile,
+  output: {
+    file: outputFile,
+    format: 'umd',
+    name: 'Turnstyle'
+  },
+  plugins: [
+    babel({ exclude: 'node_modules/**/' }),
+    resolve(),
+    commonjs({ include: 'node_modules/**' }),
+    uglify(),
+    fileSize()
+  ]
+});
 
 export default [
-  {
-    input: 'src/index.js',
-    output: {
-      file: 'dist/turnstyle.js',
-      format: 'umd',
-      name: 'turnstyle'
-    },
-    plugins: [
-      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
-      babel({ exclude: 'node_modules/**/' }),
-      resolve(),
-      commonjs({ include: 'node_modules/**' }),
-      uglify(),
-      fileSize()
-    ]
-  }
+  common({
+    inputFile: 'src/index.js',
+    outputFile: 'dist/turnstyle.js'
+  }),
+  common({
+    inputFile: 'src/server/index.js',
+    outputFile: 'dist/server.js'
+  }),
+  common({
+    inputFile: 'src/client/index.js',
+    outputFile: 'dist/client.js'
+  })
 ];
